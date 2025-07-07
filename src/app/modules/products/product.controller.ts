893849138866e5f888bb63273d763e25254e8db7
Shanto93 <shantoislam7363@gmail.com>
2025-07-08 00:02:53 +0600

    feat: Add API endpoint to create a new product

--- src/app/modules/products/product.controller.ts
+++ src/app/modules/products/product.controller.ts
@@ -1,12 +1,42 @@
 import express, { Request, Response } from "express";
 import { Product } from "./product.model";
+
 export const productRouter = express.Router();
 
+// GET all products
 productRouter.get("/", async (req: Request, res: Response) => {
-  const allData = await Product.find();
-  res.status(201).json({
-    message: "Retrived all products",
-    success: true,
-    data: allData,
-  });
+  try {
+    const allData = await Product.find();
+    res.status(200).json({
+      message: "Retrieved all products",
+      success: true,
+      data: allData,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error Retrieved all products",
+      error: error instanceof Error ? error.message : error,
+    });
+  }
+});
+
+// POST create new product
+productRouter.post("/create-product", async (req: Request, res: Response) => {
+  try {
+    const productData = req.body;
+    const newProduct = await Product.create(productData);
+
+    res.status(201).json({
+      message: "Product created successfully",
+      success: true,
+      data: newProduct,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: "Failed to create product",
+      error: error instanceof Error ? error.message : error,
+    });
+  }
 });
